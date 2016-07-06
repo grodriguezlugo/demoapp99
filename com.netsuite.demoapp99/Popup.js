64239d04eba24eabf3a15871a7eb2241fd1da97e
grlugo <grlugo@192.168.154.32>
2016-07-06 16:17:08 -0700

    1st commit

---
+++ com.netsuite.demoapp99/Popup.js
@@ -0,0 +1,19 @@
+/**
+ * Module Description
+ * 
+ * Version    Date            Author           Remarks
+ * 1.00       24 Jun 2016     grlugo
+ *
+ */
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
+ * @appliedtorecord recordType
+ *   
+ * @returns {Boolean} True to continue save, false to abort save
+ */
+function clientSaveRecord(){
+	alert('Thank you for saving this record');
+
+    return true;
+}
